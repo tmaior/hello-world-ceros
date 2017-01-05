3f4ce47fd3ab100d8ac384613d1eb42efe9fdbb5
tmaior <tlmaior@gmail.com>
2017-01-05 19:20:25 -0200

    Update index.php

--- hello-world/www/index.php
+++ hello-world/www/index.php
@@ -17,7 +17,7 @@
 <body>
 	<img id="logo" src="./logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>New Hello World 2 - Application Test For Ceros Github with Webhook - My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<?php if($_ENV["HOSTNAME"]) {?><h3>New Hello World 3 - Application Test For Ceros Github with Webhook - My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
 	foreach($_ENV as $key => $value) {
